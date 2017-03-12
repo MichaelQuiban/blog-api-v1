d5c5b67b530a7de3448ff97d896c59cb1cf7af42
TheSaintMichael <MichaelQuiban@outlook.com>
2017-03-12 14:30:26 -0700

    Server prereqs for unit tests

---
+++ test/test-server.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require ('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const should= chai.should();
+
+
+chai.use(chaiHttp);
+
+describe('Blog posts', function() {
+
+	before(function() {
+		return runServer();
+	});
+
+	after(function() {
+		return closeServer();
+	});
+});
