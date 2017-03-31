24213d5f5a470b8b3b43694052ca4e50f45df070
TheSaintMichael <MichaelQuiban@outlook.com>
2017-03-30 21:38:30 -0700

    Fixed /Models/ Schema

--- models.js
+++ models.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const blogPostSchema = mongoose.Schema( {
     "title": {type: String, required: true},
+    "content":{type: String, required: true},
     "author": {
         firstName: String,
         lastName: String,
     },
-    "content":{type: String, required: true}
 });
 
 const blogPosts = mongoose.model("BlogPosts",blogPostSchema)
