ebc0b3a74632d12659ba4758630b838ef29f3871
TheSaintMichael <MichaelQuiban@outlook.com>
2017-04-06 20:08:52 -0700

    Fixed port number

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://<testuser>:<123456>@ds143980.mlab.com:43980/blog-app';
-exports.PORT = process.env.PORT || 53710;
+                      'mongodb://testuser:123456@ds143980.mlab.com:43980/blog-app';
+exports.PORT = process.env.PORT || 8080;
